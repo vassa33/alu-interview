e536988159895cce97ec7b63e029c6936e42b236
Vanessa Peggy <61325877+vassa33@users.noreply.github.com>
2023-01-20 00:19:23 +0300

    Update 0-minoperations.py

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+import math
 """
 The function minOperations calculates the fewest number of operations
 needed to result in exactly n H characters in the file.
@@ -8,8 +9,4 @@ needed to result in exactly n H characters in the file.
 def minOperations(n):
     if n <= 0:
         return 0
-    copy_all = 0
-    while n % 2 == 0:
-        n = n // 2
-        copy_all += 1
-    return copy_all + (1 if n > 1 else 0)
+    return int(math.log2(n)) + 1
