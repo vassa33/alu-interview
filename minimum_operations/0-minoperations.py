fa5a8548f0bd89a342e8f3733f573cbe315bea56
Vanessa Peggy <61325877+vassa33@users.noreply.github.com>
2023-01-20 00:34:15 +0300

    Update 0-minoperations.py

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -13,15 +13,9 @@ def minOperations(n):
         return 0
     operations = 0
     while n > 1:
+        operations += 1
         if n % 2 == 0:
             n = n // 2
-        elif n % 3 == 0:
-            n = n // 3
-            operations += 2
-        elif n % 5 == 0:
-            n = n // 5
-            operations += 4
         else:
             n -= 1
-            operations += 1
     return operations
