08d585ba5e1f4b87c1e1116b8eb1c0aeb22463ca
Vanessa Peggy <61325877+vassa33@users.noreply.github.com>
2023-01-20 00:28:04 +0300

    Update 0-minoperations.py

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -11,4 +11,11 @@ import math
 def minOperations(n):
     if n <= 0:
         return 0
-    return math.ceil(math.log(n, 2)) + 1
+    operations = 0
+    while n > 1:
+        operations += 1
+        if n % 2 == 0:
+            n = n // 2
+        else:
+            n -= 1
+    return operations
