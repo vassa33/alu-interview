6a6d582ce7f79245f69d57ae525588441c9e4867
Vanessa Peggy <61325877+vassa33@users.noreply.github.com>
2023-01-19 22:50:25 +0300

    dsa01

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -1,1 +1,13 @@
+#!usr/bin/python3
 
+def minOperations(n):
+    if n <= 0: 
+        return 0
+    else:
+        copy_all = 0
+        while n % 2 == 0:
+            n = n // 2
+            copy_all += 1
+        return copy_all + (n - 1)
+        
+#new line
