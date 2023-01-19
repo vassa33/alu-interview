86cf4513ac494ecba08fdcc9bcb64282a9fbf8db
Vanessa Peggy <61325877+vassa33@users.noreply.github.com>
2023-01-19 22:54:59 +0300

    Update 0-minoperations.py

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
 
 def minOperations(n):
+    """
+    Calculates the fewest number of operations needed to result in exactly n H characters in the file.
+    The function can perform two operations: Copy All and Paste.
+    The function uses a greedy algorithm that repeatedly divides the target number n by 2 until it can no longer be divided. 
+    The number of times it can be divided by 2 represents the number of "Copy All" operations needed, 
+    and the final number represents the number of "Paste" operations needed.
+    """
     if n <= 0: 
         return 0
     else:
@@ -9,5 +16,3 @@ def minOperations(n):
             n = n // 2
             copy_all += 1
         return copy_all + (n - 1)
-        
-#new line
