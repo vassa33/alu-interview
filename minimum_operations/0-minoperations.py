c0c93f96ab1b347f1cba7b70a9df2b7cd838f5a6
Vanessa Peggy <61325877+vassa33@users.noreply.github.com>
2023-01-20 00:07:13 +0300

    Update 0-minoperations.py

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -6,9 +6,9 @@ The function min_operations calculates the fewest number of operations needed to
 def minOperations(n):
     if n <= 0:
         return 0
-    else:
-        copy_all = 0
-        while n % 2 == 0:
-            n = n // 2
-            copy_all += 1
-        return copy_all + n
+    copy_all = 0
+    while n % 2 == 0:
+        n = n // 2
+        copy_all += 1
+    return copy_all + n
+
