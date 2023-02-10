95dcc6b8deee1bbc582bb7b065d5e9db009fefe7
Vanessa Peggy <61325877+vassa33@users.noreply.github.com>
2023-02-10 15:08:28 +0300

    Update 0-minoperations.py

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 """Minimum Operations"""
 
+
 def minOperations(n):
     """
-    Calculates the fewest number of operations needed 
+    Calculates the fewest number of operations needed
     to result in exactly n H characters in the file.
     """
     if n <= 1:
