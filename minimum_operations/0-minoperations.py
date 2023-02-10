2b6d002b8ddbae8c0cb770429859b0f3d1483990
Vanessa Peggy <61325877+vassa33@users.noreply.github.com>
2023-02-10 14:55:02 +0300

    Optimize

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -6,13 +6,17 @@ needed to result in exactly n H characters in the file.
 
 
 def minOperations(n):
-    if n <= 0:
+    if n <= 1:
         return 0
     operations = 0
     i = 2
     while i <= n:
         while n % i == 0:
-            operations += 1
+            operations += i
             n /= i
         i += 1
+        
+    if operations == 0:
+        return n
+        
     return operations
