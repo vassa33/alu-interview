5d26762c56dfe07b5b4aedff3991330652a0454d
Vanessa Peggy <61325877+vassa33@users.noreply.github.com>
2023-02-21 16:16:17 +0300

    Update 0-rain.py

--- rain/0-rain.py
+++ rain/0-rain.py
@@ -1,4 +1,22 @@
+#!/usr/bin/python3
+"""Rain. """
+
+
 def rain(walls):
+    """
+    Calculates the amount of water retained between walls after it rains.
+
+    Args:
+    walls: A list of non-negative integers representing the heights of walls
+    with unit width 1, as if viewing the cross-section of a relief map.
+
+    Returns:
+    An integer indicating the total amount of rainwater retained between the walls.
+
+    Assumes that the ends of the list (before index 0 and after index walls[-1])
+    are not walls, meaning they will not retain water.
+    If the list is empty, returns 0.
+    """
     n = len(walls)
     if n == 0:
         return 0
@@ -19,4 +37,3 @@ def rain(walls):
         water += min(left_max[i], right_max[i]) - walls[i]
 
     return water if water >= 0 else 0
-
